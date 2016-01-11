1584b4c82845efca39b277338738d86fc42417f7
Wendy Liang <jliang@xilinx.com>
2016-01-11 01:09:35 -0800

    Use CMake for compilation
    using cmake is in order to make OpenAMP compilation
    easier to cross OSes and compilers
    
    Signed-off-by: Wendy Liang <jliang@xilinx.com>

---
+++ cmake/platforms/zynqmp_r5_generic.cmake
@@ -0,0 +1,11 @@
+set (CMAKE_SYSTEM_PROCESSOR "arm" CACHE STRING "")
+set (MACHINE                "zynqmp_r5" CACHE STRING "")
+set (CROSS_PREFIX           "armr5-none-eabi-" CACHE STRING "")
+set (CMAKE_C_FLAGS          "-mfloat-abi=soft -mcpu=cortex-r5 -O2" CACHE STRING "")
+set (CMAKE_ASM_FLAGS        "-mfloat-abi=soft -mcpu=cortex-r5" CACHE STRING "")
+set (PLATFORM_LIB_DEPS      "-lxil -lc -lm" CACHE STRING "")
+set (APP_EXTRA_C_FLAGS      "-DZYNQMP_R5" CACHE STRING "")
+
+include (cross_generic_gcc)
+
+# vim: expandtab:ts=2:sw=2:smartindent
